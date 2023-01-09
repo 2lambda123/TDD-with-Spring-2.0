484704c2b46751b5480f959251ea923c27998139
Shehroz Ali <shehroz.ali@gopayfast.com>
2023-01-09 12:38:50 +0500

    Refactored

--- src/main/java/com/example/demo/TodoService.java
+++ src/main/java/com/example/demo/TodoService.java
@@ -7,6 +7,11 @@ import java.util.ArrayList;
 @Service
 public class TodoService {
     public ArrayList<String> getAllTodos() {
-        return new ArrayList<String>();
+        ArrayList<String> todos = new ArrayList<>();
+        todos.add("Make Lunch");
+        todos.add("Charge Laptop");
+        todos.add("Pack bag");
+
+        return todos;
     }
 }
