6d32b9912e30080599050450732bfb15f2a418e8
Shehroz Ali <shehroz.ali@gopayfast.com>
2023-01-09 11:59:30 +0500

    TodoServiceTest: Injected TodoService

--- src/test/java/com/example/demo/TodoServiceTest.java
+++ src/test/java/com/example/demo/TodoServiceTest.java
@@ -1,4 +1,11 @@
+package com.example.demo;
+
+import lombok.RequiredArgsConstructor;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@RequiredArgsConstructor
 class TodoServiceTest {
-  
+    private final TodoService serviceUnderTest;
+
 }
