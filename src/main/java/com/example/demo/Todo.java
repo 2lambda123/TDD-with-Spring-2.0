723cf2296ed64e778f949d5c1401e26c34ec3c7c
Shehroz Ali <shehroz.ali@gopayfast.com>
2023-01-09 14:45:34 +0500

    Todo: Added @Entity annotation Todo: Added new id field

--- src/main/java/com/example/demo/Todo.java
+++ src/main/java/com/example/demo/Todo.java
@@ -1,15 +1,20 @@
 package com.example.demo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class Todo {
+    @Id
+    private Integer id;
     private String title;
     private String description;
 }
