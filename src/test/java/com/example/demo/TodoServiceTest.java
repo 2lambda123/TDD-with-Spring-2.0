976d8d3e978bf0c5f11076f6ac50dec734fd3b4f
Shehroz Ali <shehroz.ali@gopayfast.com>
2023-01-09 12:01:22 +0500

    TodoServiceTest: Created new test method shouldReturnAllTodos()

--- src/test/java/com/example/demo/TodoServiceTest.java
+++ src/test/java/com/example/demo/TodoServiceTest.java
@@ -1,11 +1,17 @@
 package com.example.demo;
 
 import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @RequiredArgsConstructor
 class TodoServiceTest {
     private final TodoService serviceUnderTest;
+
+    @Test
+    void shouldReturnAllTodos() {
+
+    }
 
 }
