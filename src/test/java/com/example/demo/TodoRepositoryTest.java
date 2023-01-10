50170f9f4d0dc727580f3c3b5e5ce2942e9ba0dd
Shehroz Ali <@sshehroza13@gmail.com@>
2023-01-10 10:07:27 +0000

    shouldFindTodoByTitle: Green shouldReturnNullIfTodoNotFoundByTitle: Created new test method

--- src/test/java/com/example/demo/TodoRepositoryTest.java
+++ src/test/java/com/example/demo/TodoRepositoryTest.java
@@ -1,22 +1,38 @@
 package com.example.demo;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith(MockitoExtension.class)
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
 @DataJpaTest
 class TodoRepositoryTest {
-    @InjectMocks
-    private TodoRepository respositoryUnderTest;
+    @Autowired
+    private TodoRepository repositoryUnderTest;
+
+    @BeforeEach
+    void setUp() {
+        Todo todo = new Todo(1, "Todo 1", "Clean desk");
+        repositoryUnderTest.save(todo);
+    }
 
     @Test
     @DisplayName("Should find todo by title")
     void shouldFindTodoByTitle() {
+        Todo actual = repositoryUnderTest.findTodoByTitle("Todo 1");
+        assertThat(actual).isNotNull();
+    }
+
+    @Test
+    @DisplayName("Should return null if todo not found by title")
+    void shouldReturnNullIfTodoNotFoundByTitle() {
 
     }
 }
