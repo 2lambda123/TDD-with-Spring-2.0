8631de65a06542a9983c8746e41cb2718075ea50
Shehroz Ali <@sshehroza13@gmail.com@>
2023-01-10 09:36:52 +0000

    TodoRepositoryTest: Created new test method shouldFindTodoByTitle

--- src/test/java/com/example/demo/TodoRepositoryTest.java
+++ src/test/java/com/example/demo/TodoRepositoryTest.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -11,4 +13,10 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 class TodoRepositoryTest {
     @InjectMocks
     private TodoRepository respositoryUnderTest;
+
+    @Test
+    @DisplayName("Should find todo by title")
+    void shouldFindTodoByTitle() {
+
+    }
 }
