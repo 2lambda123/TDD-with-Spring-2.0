8ab07b3ea6a7ffef5447cb9a14987bf5cb2f2731
Shehroz Ali <@sshehroza13@gmail.com@>
2023-01-10 09:35:01 +0000

    TodoRepositoryTest: Added @ExtendWith(MockitoExtension.class), @DataJpaTest annotations TodoRepositoryTest: Injected TodoRepository layer using @InjectMocks

--- src/test/java/com/example/demo/TodoRepositoryTest.java
+++ src/test/java/com/example/demo/TodoRepositoryTest.java
@@ -1,7 +1,14 @@
 package com.example.demo;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+@ExtendWith(MockitoExtension.class)
+@DataJpaTest
 class TodoRepositoryTest {
-
+    @InjectMocks
+    private TodoRepository respositoryUnderTest;
 }
