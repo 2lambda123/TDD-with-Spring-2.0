9583a9de96e01b09ed22a1ce18f1ffb4e10d3151
Shehroz Ali <@sshehroza13@gmail.com@>
2023-01-10 09:26:04 +0000

    TodoRepository: Created new custom @Query method to find Todo by title

--- src/main/java/com/example/demo/TodoRepository.java
+++ src/main/java/com/example/demo/TodoRepository.java
@@ -1,8 +1,11 @@
 package com.example.demo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TodoRepository extends JpaRepository<Todo, Integer> {
+    @Query
+    Todo findTodoByTitle(String title);
 }
